e60b4d9c6e85e4d47ce975bcd31b4ef82a307925
AntonyIS <antonyshikubu@gmail.com>
2023-06-30 14:05:08 +0300

    MC

--- internal/adapters/repostory/dynamodb/dynamodb.go
+++ internal/adapters/repostory/dynamodb/dynamodb.go
@@ -1,19 +1,72 @@
+/*
+Package name : repository
+File name : dynamodb.go
+Author : Antony Injila
+Description :
+	- Host dynamoDb database specific methods
+*/
+
 package dynamodb
 
-import "github.com/AntonyIS/car-rent-user-mgmt-svc/internal/core/domain"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+
+
+    "fmt"
+
+	"github.com/AntonyIS/car-rent-user-mgmt-svc/internal/core/domain"
+)
+
+
 
 type DynamoDBClient struct {
-	client string
+	client *dynamodb.DynamoDB
+	tableName string
 }
 
-func NewDynamoDBClient() *DynamoDBClient {
+func NewDynamoDBClient(tableName string) *DynamoDBClient {
+	creds := credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), "")
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String(c.Region),
+		Credentials: creds,
+	}))
+	// Create DynamoDB client
+	client := dynamodb.New(sess)
 	return &DynamoDBClient{
-		client: "client",
+		client: client,
+		tableName : tableName,
 	}
 }
 
 func (db *DynamoDBClient) CreateUser(user *domain.User) (*domain.User, error) {
-	return db.CreateUser(user)
+	entityParsed, err := dynamodbattribute.MarshalMap(user)
+	if err != nil {
+		return nil, err
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      entityParsed,
+		TableName: aws.String(db.tableName),
+	}
+
+	_, err = db.client.PutItem(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user, err = db.ReadUser(user.id)
+	if err != nil {
+		return nil, errs.Wrap(errors.New(fmt.Sprintf("%s: %s", internalServerError, err)), "adapters.repository.dynamodb.CreateUser")
+	}
+
+	return user, nil
 }
 
 func (db *DynamoDBClient) ReadUser(id string) (*domain.User, error) {
